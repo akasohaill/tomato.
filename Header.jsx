d8bf6eeb433018a8e63158daad525063050091bf
Md Sohail Ansari <121673553+akasohaill@users.noreply.github.com>
2024-04-22 22:53:32 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite food here</h2>
+            <p>Choose from a diverse menu featuring a detectable array of dishes.Our mission is to satisfy you cravings and elevate your dinning experience, one delecious meal at a time</p>
+            <button>view menu</button>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Header
